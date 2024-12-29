3a586c5594491c30b67b5a5c8d1799ba96b09695
Scoutsiyo <117695786+Scoutsiyo@users.noreply.github.com>
2024-12-29 18:33:10 -0500

    cancer

--- p.lua
+++ p.lua
@@ -1,2 +1,13 @@
-local a = 12
-print(a + 1)
+function suma(a, b)
+    sumar = (a + b)
+    print(sumar)
+    end
+
+suma(5,5)
+function mult(a, b)
+    multiplicar = (a * b)
+    print(multiplicar)
+    end
+
+mult(5,5)
+
