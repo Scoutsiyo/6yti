99ad6c1f7f35a4fd1a992d846e79c7747e4f79c1
Scoutsiyo <117695786+Scoutsiyo@users.noreply.github.com>
2024-12-29 18:26:15 -0500

    test

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("Hello World v2")
+newlist = []
+def sumador():
+    while True:
+        try:
+            a = int(input('Enter a number (To finish, enter a string): '))
+            newlist.append(a)
+        except ValueError:
+            print(f"suma de todos {sum(newlist)}")
+            break
+sumador()
